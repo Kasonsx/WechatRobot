23eab606c3102871e965c3e6adc359f4881780b8
Kasonsx <sxk644653293@163.com>
2018-12-01 19:21:40 +0800

    upload code

---
+++ itchatRobot.py
@@ -0,0 +1,69 @@
+import itchat
+from itchat.content import *
+import requests
+#from itchat.content import *
+API_KEY = 'd1cfba0fde3c44208c1973cb4b88ab51'
+
+def response(msg):#robot reply
+	apiUrl = 'http://www.tuling123.com/openapi/api'
+	data = {
+		'key':API_KEY,
+		'info':msg,
+		'userid':'robot'
+	}
+	try:
+		r = requests.post(apiUrl, data=data).json()
+		code = r.get('code')
+		if code == 100000: #文本类 "text"就是回复内容
+			result = r.get('text')
+		elif code == 200000: #链接类 实际内容在"url"中
+			result = "{0}\n{1}".format(r.get('text'),r.get('url'))
+		return result
+	except:
+		print('server has encountered a problem.')
+
+@itchat.msg_register(TEXT)
+def auto_reply(msg):
+	#print(msg.isAt)
+	default_reply = 'I received:' + msg['Text']
+	print(msg.FromUserName,':',msg['Text'])
+	if msg.text == '沙雕图':
+		msg.user.send('你是女娲捏出来的吗？')
+	else:
+		reply = response(msg['Text'])
+		msg.user.send(reply or default_reply)
+
+@itchat.msg_register(TEXT, isGroupChat = True)
+def group_reply(msg):
+	if msg.isAt:
+		default_reply = 'I received:' + msg['Text']
+		print(msg.FromUserName,':',msg['Text'])
+		if msg.text == '沙雕图':
+			msg.user.send('你是女娲捏出来的吗？')
+		else:
+			reply = response(msg['Text'])
+			msg.user.send(reply or default_reply)
+
+def lc():#loginCallback
+	print('login successfully!')
+def ec():#exitCallback
+	print('exit!')
+
+itchat.auto_login(hotReload=True)
+# memberList = itchat.update_chatroom('差点找不到的计机4', detailedMember=True)
+# print(memberList)
+# itchat.search_chatrooms(name='差点找不到的计机4')
+@itchat.msg_register(SYSTEM)
+def get_uin(msg):
+    if msg['SystemInfo'] != 'uins': return
+    ins = itchat.instanceList[0]
+    fullContact = ins.memberList + ins.chatroomList + ins.mpList
+    print('** Uin Updated **')
+    for username in msg['Text']:
+        member = itchat.utils.search_dict_list(
+            fullContact, 'UserName', username)
+        print(('%s: %s' % (
+            member.get('NickName', ''), member['Uin']))
+            .encode(sys.stdin.encoding, 'replace'))
+
+itchat.run()
